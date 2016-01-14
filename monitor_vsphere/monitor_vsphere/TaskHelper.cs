f65ef30a44fd3f3cc4a6bc8d13339a778a725ee0
xulai1001 <xulai1001@gmail.com>
2016-01-14 15:04:59 +0800

    Jan 14

--- monitor_vsphere/monitor_vsphere/TaskHelper.cs
+++ monitor_vsphere/monitor_vsphere/TaskHelper.cs
@@ -26,6 +26,7 @@ namespace monitor_vsphere
                     task_ref, new string[] { "info.state", "info.error" }, new string[] { "state" },
                     new object[][] { new object[] { TaskInfoState.success, TaskInfoState.error } });
                 message = (string)result[1];
+                if (message != null) Console.WriteLine(message);
                 return (result[0].Equals(TaskInfoState.success));
             }
             catch (Exception e)
@@ -72,15 +73,21 @@ namespace monitor_vsphere
             PropHelper.BuildPropMetric();
         }
 
-        public static void BeginTask(ManagedObjectReference task, Dictionary<string, dynamic> tags = null)
+        public static VMTask BeginTask(ManagedObjectReference task, Dictionary<string, dynamic> tags = null, bool callback=true)
         {
             if (task != null)
             {
                 VMTask worker = WaitForTask;
+                if (tags == null) tags = new Dictionary<string, dynamic>();
                 tags["task"] = task;
                 tags["delegate"] = worker;
-                worker.BeginInvoke(task, TaskCompleted, tags);
+                if (callback)
+                    worker.BeginInvoke(task, TaskCompleted, tags);
+                else
+                    worker.BeginInvoke(task, null, tags);
+                return worker;
             }
+            return null;
         }
 
         public static void ProcessTask(Dictionary<string, string> args)
@@ -108,6 +115,13 @@ namespace monitor_vsphere
             info["vm"] = vmref;
             info["host"] = hostref;
             Console.WriteLine("JID: {0} 操作: {1} VM: {2} 主机: {3}", job_id, op, vm_name, hostref.Value);
+
+            // migrate args
+            string pool_name, target_name, ds_name;
+            ManagedObjectReference poolref, targetref, dsref;
+            VirtualMachinePowerState st = VirtualMachinePowerState.poweredOn;
+            VirtualMachineMovePriority pr = VirtualMachineMovePriority.defaultPriority;
+            VirtualMachineConfigSpec conf_spec;
             try
             {
                 switch (op)
@@ -148,17 +162,79 @@ namespace monitor_vsphere
                         break;
 
                     case "migrate":
-                        string pool_name = args["pool"];
-                        string target_name = args["target"];
-                        var poolref = PropHelper.get_moref_by_name(pool_name);
-                        var targetref = PropHelper.get_moref_by_name(target_name);
+                        pool_name = args["pool"];
+                        target_name = args["target"];
+                        poolref = PropHelper.get_moref_by_name(pool_name);
+                        targetref = PropHelper.get_moref_by_name(target_name);
                         info["pool"] = poolref;
                         info["target"] = targetref;
-                        VirtualMachinePowerState st = VirtualMachinePowerState.poweredOn;
-                        VirtualMachineMovePriority pr = VirtualMachineMovePriority.defaultPriority;
 
                         task = VCenter.service.MigrateVM_Task(vmref, poolref, targetref, pr, st, true);
                         BeginTask(task, info);
+                        break;
+                    case "relocate":
+                        pool_name = args["pool"];
+                        target_name = args["target"];
+                        ds_name = args["datastore"];
+                        poolref = PropHelper.get_moref_by_name(pool_name);
+                        targetref = PropHelper.get_moref_by_name(target_name);
+                        dsref = PropHelper.get_moref_by_name(ds_name);
+                        info["pool"] = poolref;
+                        info["target"] = targetref;
+                        info["ds"] = dsref;
+                        task = VCenter.service.RelocateVM_Task(vmref,
+                            new VirtualMachineRelocateSpec() { datastore = dsref, host = targetref, pool = poolref }, pr, true
+                            );
+                        BeginTask(task, info);
+                        break;
+                    case "check_migrate":
+                        pool_name = args["pool"];
+                        target_name = args["target"];
+                        poolref = PropHelper.get_moref_by_name(pool_name);
+                        targetref = PropHelper.get_moref_by_name(target_name);
+                        info["pool"] = poolref;
+                        info["target"] = targetref;
+                        info["result"] = (QueryVMotion(vmref, hostref, targetref) && CheckMigrate(vmref, targetref, poolref));
+                        Console.WriteLine("*** 操作完成 *** ");
+                        SendResult(ConvertResult(info));
+                        break;
+                    case "check_relocate":
+                        pool_name = args["pool"];
+                        target_name = args["target"];
+                        ds_name = args["datastore"];
+                        poolref = PropHelper.get_moref_by_name(pool_name);
+                        targetref = PropHelper.get_moref_by_name(target_name);
+                        dsref = PropHelper.get_moref_by_name(ds_name);
+                        info["pool"] = poolref;
+                        info["target"] = targetref;
+                        info["ds"] = dsref;
+                        info["result"] = (QueryVMotion(vmref, hostref, targetref) && CheckRelocation(vmref, targetref, poolref, dsref));
+                        Console.WriteLine("*** 操作完成 *** ");
+                        SendResult(ConvertResult(info));
+                        break;
+                    case "reconf_mem":
+                        conf_spec = new VirtualMachineConfigSpec() {
+                            memoryAllocation = GetShares("custom", int.Parse(args["value"]))
+                        };
+                        info["result"] = WaitForTask(VCenter.service.ReconfigVM_Task(vmref, conf_spec));
+                        Console.WriteLine("*** 操作完成 *** ");
+                        SendResult(ConvertResult(info));
+                        break;
+
+                    case "reconf_numcpu":
+                        conf_spec = new VirtualMachineConfigSpec()
+                        {
+                            numCPUs = int.Parse(args["value"])
+                        };
+                        info["result"] = WaitForTask(VCenter.service.ReconfigVM_Task(vmref, conf_spec));
+                        Console.WriteLine("*** 操作完成 *** ");
+                        SendResult(ConvertResult(info));
+                        break;
+                    case "reconf_disk":
+                        vd_spec = GetVDiskSpec();
+                        info["result"] = WaitForTask(VCenter.service.);
+                        Console.WriteLine("*** 操作完成 *** ");
+                        SendResult(ConvertResult(info));
                         break;
                 }
             }
@@ -168,6 +244,44 @@ namespace monitor_vsphere
                 info["error"] = e.Message;
                 SendResult(ConvertResult(info));
             }
+        }
+
+        public static bool QueryVMotion(ManagedObjectReference vmref, ManagedObjectReference host, ManagedObjectReference target)
+        {
+            return WaitForTask(VCenter.service.QueryVMotionCompatibilityEx_Task(
+                VCenter.sic.vmProvisioningChecker,
+                new ManagedObjectReference[] { vmref },
+                new ManagedObjectReference[] { host, target }
+                ));
+        }
+
+        public static bool CheckMigrate(ManagedObjectReference vmref, ManagedObjectReference target, ManagedObjectReference poolref)
+        {
+            return WaitForTask(VCenter.service.CheckMigrate_Task(
+                VCenter.sic.vmProvisioningChecker, vmref, target, poolref, VirtualMachinePowerState.poweredOff, false, null));
+        }
+
+        public static bool CheckRelocation(ManagedObjectReference vmref, ManagedObjectReference host,
+                                           ManagedObjectReference poolref, ManagedObjectReference dsref)
+        {
+            return WaitForTask(VCenter.service.CheckRelocate_Task(
+                VCenter.sic.vmProvisioningChecker, vmref, 
+                new VirtualMachineRelocateSpec() { datastore = dsref, host = host, pool = poolref },
+                null));
+        }
+
+        public static ResourceAllocationInfo GetShares(string level="custom", int value=0)
+        {
+            List<string> levels = new List<string>() { "low", "normal", "high", "custom" };
+            return new ResourceAllocationInfo() {
+                shares = new SharesInfo() { level = (SharesLevel)levels.IndexOf(level), shares = value }
+            };
+        }
+
+        public static VirtualDiskConfigSpec GetVDiskSpec(ManagedObjectReference vmref)
+        {
+            VirtualDiskConfigSpec ret = new VirtualDiskConfigSpec();
+            VirtualMachineConfigInfo conf = (VirtualMachineConfigInfo)VCenter.su.GetDynamicProperty(vmref, "config");
         }
 
     }
